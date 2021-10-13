a9d64919ac3134748633e0ec09fdc7f530ee9143
Jose Manuel García <josemanu16@correo.ugr.es>
2021-10-13 16:17:04 +0200

    Actualizo venta para poder vender más de un producto

--- src/venta.ts
+++ src/venta.ts
@@ -1,38 +1,71 @@
 import {Producto} from './producto'
 
+//Como estaba antes, solo podia tener un producto y una cantidad
+//Tiene mas sentido que pueda añadir tantos productos como quiera, como si fuese una factura
+//La fecha si que es común a toda la factura
 
 export class Venta {
 
-    private _producto:Producto;
-    private _cantidad:number;
+    private _productos: [producto:Producto, cantidad:number][]
     private _fecha:string;
 
-    constructor (producto:Producto, cantidad:number, fecha:string) {
-
-        this._producto = producto;
-        this._cantidad = cantidad;
+    constructor (fecha:string) {
         this._fecha = fecha;
-        //la cantidad será modificada en el gestor, ya que al objeto venta no le corresponde saber que ocurre en el objeto existencias
+        //La cantidad será modificada en el gestor, ya que al objeto venta no le corresponde saber que ocurre en el objeto existencias
+    }
+
+    public get fecha() {
+
+        return this._fecha;
 
     }
 
-    //métodos get
-    public get producto() {
-
-        return this._producto;
-
-    }
-
-    public get cantidad() {
-
-        return this._cantidad;
+    add_venta(producto:Producto, cantidad:number) {
+        
+        this._productos.push([producto,cantidad])
 
     }
 
-    public get todos_datos() {
+    get_producto(id:string) {
 
-        return "Se vendieron " + this._cantidad +  "de " + this._producto.nombre + "el" + this._fecha;
+        let seguir:boolean = true;
+        let indice = -1;
 
+        for (let i = 0; this._productos.length && seguir; i++) {
+
+            indice = i;
+            let id_producto = this._productos[indice][0].id_producto;
+
+            if (id_producto == id) {
+
+                seguir = false;
+
+            }
+
+        }
+
+        return this._productos[indice][0];
+    }
+
+    get_id_producto(id:string) {
+
+        let seguir:boolean = true;
+        let indice = -1;
+
+        for (let i = 0; this._productos.length && seguir; i++) {
+
+            indice = i;
+            let id_producto = this._productos[indice][0].id_producto;
+
+            if (id_producto == id) {
+
+                seguir = false;
+
+            }
+
+        }
+
+        return this._productos[indice][0].id_producto;
     }
 
 }

eff9a2cc383c5a3d6957b1b2a9d58efe395e931a
Jose Manu <jmgl.rm@gmail.com>
2021-10-15 13:15:29 +0200

    Actualizo clase producto con lo hablado para avanzar en #9

--- src/producto.ts
+++ src/producto.ts
@@ -1,27 +1,78 @@
+import { Constantes } from "./constantes"
+import { Error_producto } from "./errores"
+import { Tipo_producto } from "./tipo_producto";
+
 export class Producto {
-
+    
+    private _id_producto:number;
     private _nombre:string;
-    private _pvp:number;
+    private _marca:string;
+    private _tipo:Tipo_producto;
 
+    constructor(id_producto:number, nombre:string, marca:string, tipo:Tipo_producto) {
 
-    // constructor, le pasamos los parámetros de prodcuto y creamos la clase existencia asociada al mismo
-    constructor(nombre:string, precio:number) {
+        if(id_producto <= Constantes.ID_INVALIDO) {
 
+            throw new Error_producto("Se intenta crear el producto con un ID no válido");
+
+        }
+        this._id_producto = id_producto;
+
+        if (!nombre) {
+
+            throw new Error_producto("Se intentó crear un producto con ID = " + id_producto + " sin nombre");
+
+        } else if (nombre.length < Constantes.LON_NOMBRE_MIN) {
+
+            throw new Error_producto("Se intentó crear un producto con ID " + id_producto + " con un nombre demasido corto");
+
+        } else if (nombre.length > Constantes.LON_NOMBRE_MAX) {
+
+            throw new Error_producto("Se intentó crear un producto con ID " + id_producto + " con un nombre demasido largo");
+
+        }
         this._nombre = nombre;
-        this._pvp = precio;
+
+        if(!marca) {
+
+            throw new Error_producto("Se intentó crear un producto con ID " + id_producto + " y nombre \'" + nombre + "\' sin marca");
+
+        } else if (marca.length < Constantes.LON_MARCA_MIN) {
+
+            throw new Error_producto("Se intentó crear un producto con ID " + id_producto + " y nombre \'" + nombre + "\' con una marca muy corta");
+
+        } else if (marca.length > Constantes.LON_MARCA_MAX) {
+
+            throw new Error_producto("Se intentó crear un producto con ID " + id_producto + " y nombre \'" + nombre + "\' con una marca muy larga");
+
+        }
+        this._marca = marca;
+
+        this._tipo = tipo;
+    }
+
+    //accesores
+    public get id_prodcuto() { 
+
+        return this._id_producto;
 
     }
 
-    //metodos get
-    public get nombre (): string {
+    public get nombre() { 
 
-        return this.nombre;
+        return this._nombre;   
 
     }
 
-   public get pvp (): number {
+    public get marca() { 
+        
+        return this._marca;
 
-        return this.pvp;
+    }
+
+    public get tipo() { 
+
+        return this._tipo;
 
     }
 

b09015cfd8301af8fc4130f4f8dcb4b681eef0db
José Alberto Gómez García <modej@correo.ugr.es>
2021-10-22 10:35:18 +0200

    Update with documentation

--- src/errores.ts
+++ src/errores.ts
@@ -1,4 +1,11 @@
+/**
+ * Clase para gestionar los posibles errores que puedan producerse al construir productos
+ */
 export class Error_producto extends Error {
+    /**
+     * Constructor del objeto de error
+     * @param msg Mensaje explicativo con el motivo del error
+     */
     constructor(msg: string) {
         super(msg)
     }

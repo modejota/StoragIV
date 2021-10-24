62720081744863c713dd4be9cac7e8d69658eec3
José Alberto Gómez García <modej@correo.ugr.es>
2021-10-24 20:03:18 +0200

    Update doc

--- src/existencias.ts
+++ src/existencias.ts
@@ -1,7 +1,8 @@
 import { Producto } from "./producto";
 
 /**
- * Representa el inventario del negocio
+ * Representa el inventario del negocio a través de una tabla hash.
+ * La clave será el ID del producto (codigo de barras), mientras que el valor será una pareja del producto y cantidad almacenada.
  * @public
  */
 export class Existencias {
@@ -9,7 +10,7 @@ export class Existencias {
 
     /**
      * Constructor del objeto existencias
-     * Crea un inventario sin objetos
+     * Crea un inventario sin productos
      */
     constructor() {
         this._inventario = new Map<number, [Producto, number]>()

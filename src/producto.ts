7714ca6ee4727ea1f15920f89dc2b08b95fbc321
José Alberto Gómez García <modej@correo.ugr.es>
2021-11-11 18:38:51 +0100

    Fix style - closes #20

--- src/producto.ts
+++ src/producto.ts
@@ -26,50 +26,40 @@ export class Producto {
     constructor(id_producto:number, PVP:number, nombre:string, marca:string, tipo:Tipo_producto) {
 
         if(id_producto <= Constantes.ID_INVALIDO) {
-
             throw new Error_producto("Se intenta crear el producto con un ID no válido");
-
         }
         this._id_producto = id_producto;
 
 
         if (!nombre) {
-
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto} sin nombre `);
 
         } else if (nombre.length < Constantes.LON_NOMBRE_MIN) {
-
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto} con un nombre demasido corto `);
 
         } else if (nombre.length > Constantes.LON_NOMBRE_MAX) {
-
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto} con un nombre demasido largo `);
 
         }
         this._nombre = nombre;
 
         if(!marca) {
-
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto} y nombre ${nombre} sin marca `);
 
         } else if (marca.length < Constantes.LON_MARCA_MIN) {
-
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto} y nombre ${nombre} con una marca muy corta `);
 
         } else if (marca.length > Constantes.LON_MARCA_MAX) {
-
             throw new Error_producto(` Se intentó crear un producto con ID "${id_producto} y nombre ${nombre} con una marca muy larga`);
 
         }
         this._marca = marca;
 
         if(PVP <= Constantes.CANTIDAD_INVALIDA) {
-            
             throw new Error_producto(` Se intentó crear un producto con ID ${id_producto}, nombre ${nombre}, marca ${marca} y PVP inválido `);
         
         }
         this._PVP = PVP;
-
 
         this._tipo = tipo;
     }
@@ -79,9 +69,7 @@ export class Producto {
      * @returns ID único del producto
      */
     public get id_producto() { 
-
         return this._id_producto;
-
     }
 
     /**
@@ -89,9 +77,7 @@ export class Producto {
      * @returns Nombre del producto
      */
     public get nombre() { 
-
         return this._nombre;   
-
     }
 
     /**
@@ -99,9 +85,7 @@ export class Producto {
      * @returns Marca/fabricante del producto
      */
     public get marca() { 
-        
         return this._marca;
-
     }
 
     /**
@@ -109,9 +93,7 @@ export class Producto {
      * @returns Tipo de producto
      */
     public get tipo() { 
-
         return this._tipo;
-
     }
 
     /**

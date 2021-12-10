eb4bdef3b7527e612f72ff480fa17a0682b71523
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-10 20:54:03 +0100

    Fix paths

--- src/logger.ts
+++ src/logger.ts
@@ -3,11 +3,12 @@ import * as fs from 'fs'
 
 const configuration = new Configuration()
 const log_directory = configuration.log_directory
+const log_file_path = configuration.log_file_path
 
 if(!fs.existsSync(log_directory)) {
-     fs.mkdirSync(log_directory,766)
+     fs.mkdirSync(log_directory,{ recursive: true })
 }
 
 const pino = require('pino')
-const destination = pino.destination(log_directory)
+const destination = pino.destination(log_file_path)
 export const logger = pino(destination)

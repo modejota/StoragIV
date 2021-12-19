6d9f74ecbbe0d15c3a0856dd662f1770719d4773
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-19 23:39:21 +0100

    Add other routes
    Se añaden plugins con el resto de rutas de la API

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,19 @@
 import { FastifyInstance } from "fastify";
+import productController from "./controller/productController";
+import almacenController from "./controller/almacenController";
+import facturaController from "./controller/facturaController";
+
 export default async function router(fastify: FastifyInstance) {
-    
+
+    fastify.register(productController)
+    fastify.register(almacenController)
+    fastify.register(facturaController)
+
+    fastify.get('/', async (request,reply) => {
+        reply.status(200).send({greeting: 'Welcome to storagIV API'});
+    })
+
     fastify.get('/status', async (request,reply) => {
-        reply.status(201).send({hello: 'I am live!'});
+        reply.status(200).send({hello: 'I am live!'});
     })
 }

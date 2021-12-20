4f9455d742e25764cae32532fa3c7524ebbd4192
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-20 16:52:53 +0100

    First routes prototype without schemas for storage

---
+++ src/controller/almacenController.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+
+export default async function almacenController(fastify:FastifyInstance) {
+    
+    fastify.get('/',async function (request, reply) {
+        
+    })
+
+    fastify.post('/',async (request, reply) => {
+        
+    })
+
+    fastify.get('/:id',async function (request, reply) {
+        
+    })
+
+    fastify.delete('/:id',async (request, reply) => {
+        
+    })
+
+    fastify.put('/:id',async (request, reply) => {
+        
+    })
+
+    fastify.get('/size',async (request, reply) => {
+
+    })
+
+}

81755b7543b0e5ac42dd4f8860383b70281cd6b8
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-20 19:16:26 +0100

    Optimized way
    Se modifica para que se validen los datos del cuerpo de la solicitud y se optimice la serializacion

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,18 +1,32 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, RouteShorthandOptions } from "fastify";
+import { Constantes as C} from '../constantes';
 
-interface ProductParams {
-    id: number
-    nombre: string
-    marca: string
-    tipo: number
-    PVP: number
-    cantidad?: number
+const ProductData = {
+    type: 'object',
+    required: ['id','nombre','marca','tipo','PVP'],
+    properties: {
+        id: { type: 'number', minimum: C.ID_INVALIDO+1},
+        nombre: { type: 'string', minLength: C.LON_NOMBRE_MIN, maxLength: C.LON_NOMBRE_MAX},
+        marca: { type: 'string', minLength: C.LON_MARCA_MIN, maxLength: C.LON_NOMBRE_MAX},
+        tipo: { type: 'number', minimum: C.ID_INVALIDO},
+        PVP: {type: 'number', minimum: C.ID_INVALIDO},
+    },
 }
 
 export default async function productController(fastify:FastifyInstance) {
     
-    fastify.post<{Body: ProductParams}>('/',async function (request,reply) {
-
+    fastify.route({
+        method: 'POST',
+        url: '/',
+        schema: {
+            body: ProductData,
+            response: {
+                201: ProductData
+            }
+        },
+        handler: function (request,reply) {
+            // TO DO
+        }
     })
 
 }

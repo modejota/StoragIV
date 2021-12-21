a7efd56c7d11c0b06cd9f6fe9132f34a3d201ca4
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-21 19:22:52 +0100

    Rename routes to clarify

--- src/router.ts
+++ src/router.ts
@@ -4,8 +4,8 @@ import facturaController from "./controller/facturaController";
 
 export default async function router(fastify: FastifyInstance) {
 
-    fastify.register(almacenController, { prefix: '/storage' })
-    fastify.register(facturaController, { prefix: '/bills' })
+    fastify.register(almacenController, { prefix: '/product' })
+    fastify.register(facturaController, { prefix: '/bill' })
 
     fastify.get('/', async (request,reply) => {
         reply.status(200).send({greeting: 'Welcome to storagIV API'});

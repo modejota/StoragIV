08db3aaa0b45ead3308261d0c7e24da47a87cae5
José Alberto Gómez García <modej@correo.ugr.es>
2021-12-29 13:12:39 +0100

    #36 First version

--- src/controller/almacenController.ts
+++ src/controller/almacenController.ts
@@ -1,19 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { Constantes as C} from '../constantes';
-
-
-const ProductData = {
-    type: 'object',
-    required: ['id','nombre','marca','tipo','PVP'],
-    properties: {
-        id: { type: 'number', minimum: C.ID_INVALIDO+1},
-        nombre: { type: 'string', minLength: C.LON_NOMBRE_MIN, maxLength: C.LON_NOMBRE_MAX},
-        marca: { type: 'string', minLength: C.LON_MARCA_MIN, maxLength: C.LON_NOMBRE_MAX},
-        tipo: { type: 'number', minimum: C.ID_INVALIDO},
-        PVP: {type: 'number', minimum: C.ID_INVALIDO},
-        cantidad: {type: 'number', minimum: C.ID_INVALIDO}
-    },
-}
+import { handler } from "../handler";
+import { ConstantesValidacion as CV } from "../constantes/constantesValidacion";
 
 export default async function almacenController(fastify:FastifyInstance) {
     
@@ -21,34 +8,101 @@ export default async function almacenController(fastify:FastifyInstance) {
         method: 'POST',
         url: '/',
         schema: {
-            body: ProductData,
-            response: {
-                201: ProductData
-            }
+            body: CV.ProductData
         },
-        handler: function (request,reply) {
-            // TO DO
+        handler: async function (request,reply) {
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                let product = handler.crear_producto(data.id, data.nombre, data.marca, data.tipo, data.PVP)
+                handler.aniadir_producto_almacen(product,data.cantidad)
+                reply.status(201).send({result: "Product added successfully to storage."})
+            } catch {
+                reply.status(500).send({error: `Product with ID ${data.id} already existing. Can't create.`})
+            }
         }
     })
 
-    fastify.get('/',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/',
+        handler: async function (request, reply) {
+            const result = Object.fromEntries(handler.get_all_productos_almacen())
+            reply.status(200).send(result)
+        }
     })
 
-    fastify.get('/:id',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/:id',
+        schema: {
+            params: CV.ProductID
+        },
+        handler: async function (request, reply) {
+            let data = JSON.parse(JSON.stringify(request.params))
+            try {
+                let product = handler.obtener_producto_almacen(data.id)
+                reply.code(200).send(product)
+            } catch {
+                reply.status(404).send({error: `Product with ID ${data.id} not found.`})
+            }
+        }
     })
 
-    fastify.delete('/:id',async (request, reply) => {
-        
+    fastify.route({
+        method: 'DELETE',
+        url: '/:id',
+        schema: {
+            params: CV.ProductID
+        },
+        handler: async function (request, reply) {
+            let data = JSON.parse(JSON.stringify(request.params))
+            try {
+                handler.eliminar_producto_almacen(data.id)
+                reply.code(200).send({result: `Product with ID ${data.id} deleted successfully.`})
+            } catch {
+                reply.status(404).send({error: `Product with ID ${data.id} not found.`})
+            }
+        }
     })
 
-    fastify.put('/:id',async (request, reply) => {
-        
+    fastify.route({
+        method: 'PUT',
+        url: '/:id',
+        schema: {
+            params: CV.ProductID,
+            body: CV.ModifyingProductData
+        },
+        handler: async function (request, reply) {
+            let ID = JSON.parse(JSON.stringify(request.params)).id
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                let product = handler.crear_producto(ID, data.nombre, data.marca, data.tipo, data.PVP)
+                handler.eliminar_producto_almacen(ID)
+                handler.aniadir_producto_almacen(product,data.cantidad)
+                reply.code(200).send({result: `Product with ID ${ID} updated successfully.`})
+            } catch {
+                reply.code(404).send({error: `Product with ID ${ID} not found.`})
+            }
+        }
     })
 
-    fastify.get('/size',async (request, reply) => {
-
+    fastify.route({
+        method: 'PATCH',
+        url: '/:id',
+        schema: {
+            params: CV.ProductID,
+            body: CV.QuantityOfAProductData
+        },
+        handler: async function (request, reply) {
+            let ID = JSON.parse(JSON.stringify(request.params)).id
+            let new_c = JSON.parse(JSON.stringify(request.body)).cantidad
+            try {
+                handler.actualizar_cantidad_producto_almacen(ID,new_c)
+                reply.code(200).send({result: `Quantity of product with ID ${ID} updated successfully.`})
+            } catch {
+                reply.code(404).send({error: `Product with ID ${ID} not found.`})
+            }
+        }
     })
 
 }
--- src/controller/facturaController.ts
+++ src/controller/facturaController.ts
@@ -1,48 +1,179 @@
 import { FastifyInstance } from "fastify";
+import { handler } from "../handler";
+import { ConstantesValidacion as CV } from "../constantes/constantesValidacion";
+import { parseFactura, parseFacturas } from "../parsers";
 
 export default async function facturaController(fastify:FastifyInstance) {
     
-    fastify.get('/',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/',
+        handler: function (request, reply) {
+            const result = parseFacturas(handler.get_all_facturas())
+            reply.status(200).send(result)
+        }
+    })
+    
+    fastify.route({
+        method: 'POST',
+        url: '/',
+        schema: {
+            body: CV.BillID 
+        },
+        handler: function (request, reply) {
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                handler.crear_factura(data.id)
+                reply.code(201).send({result: `Bill with ID ${data.id} created successfully.`})
+            } catch {
+                reply.code(500).send({error: `Bill with ID ${data.id} already existing. Can't create.`})
+            }
+        }
     })
 
-    fastify.get('/size',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/:id',
+        schema: {
+            params: CV.BillID
+        },
+        handler: function (request, reply) {
+            let data = JSON.parse(JSON.stringify(request.params))
+            try {
+                let bill = handler.obtener_factura(data.id)
+                reply.code(200).send(parseFactura(bill))
+            } catch {
+                reply.status(404).send({error: `Bill with ID ${data.id} not found.`})
+            }
+        }
     })
 
-    fastify.post('/',async function (request, reply) {
-        
+    fastify.route({
+        method: 'DELETE',
+        url: '/:id',
+        schema: {
+            params: CV.BillID
+        },
+        handler: function (request, reply) {
+            let data = JSON.parse(JSON.stringify(request.params))
+            try {
+                handler.eliminar_factura(data.id)
+                reply.code(200).send({result: `Bill with ID ${data.id} deleted successfully.`})
+            } catch {
+                reply.status(404).send({error: `Bill with ID ${data.id} not found.`})
+            }
+        }
     })
 
-    fastify.get('/:id',async function (request, reply) {
-        
+    fastify.route({
+        method: 'POST',
+        url: '/:id',
+        schema: {
+            params: CV.BillID,
+            body: CV.ProductData
+        },
+        handler: function (request, reply) {
+            let id = JSON.parse(JSON.stringify(request.params)).id
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                let product = handler.crear_producto(data.id, data.nombre, data.marca, data.tipo, data.PVP)
+                handler.aniadir_producto_factura(id,product,data.cantidad)
+                reply.status(201).send({result: `Product added successfully to bill with ID ${id}.`})
+            } catch {
+                reply.status(500).send({error: `Product with ID ${data.id} already existing in bill with ID ${id}. Can't create.`})
+            }
+        }
     })
 
-    fastify.delete('/:id',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/:id/product/:idp',
+        schema: {
+            params: CV.ProductOfABillID
+        },
+        handler: function (request, reply) {
+            let params = JSON.parse(JSON.stringify(request.params))
+            try {
+                let product = handler.obtener_producto_factura(params.id, params.idp)
+                reply.code(200).send(product)
+            } catch {
+                reply.status(404).send({error: "Product and/or bill not found."})
+            }
+        }
     })
 
-    fastify.post('/:id',async function (request, reply) {
-        
+    fastify.route({
+        method: 'DELETE',
+        url: '/:id/product/:idp',
+        schema: {
+            params: CV.ProductOfABillID
+        },
+        handler: function (request, reply) {
+            let params = JSON.parse(JSON.stringify(request.params))
+            try {
+                handler.eliminar_producto_factura(params.id, params.idp)
+                reply.code(200).send({result: `Product with ID ${params.idp} deleted successfully from bill with ID ${params.id}.`})
+            } catch {
+                reply.status(404).send({error: "Product and/or bill not found."})
+            }
+        }
     })
 
-    fastify.get('/:id/product/:idp',async function (request, reply) {
-        
+    fastify.route({
+        method: 'PUT',
+        url: '/:id/product/:idp',
+        schema: {
+            params: CV.ProductOfABillID,
+            body: CV.ModifyingProductData
+        },
+        handler: function (request, reply) {
+            let params = JSON.parse(JSON.stringify(request.params))
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                handler.eliminar_producto_factura(params.id,params.idp)
+                let product = handler.crear_producto(params.idp, data.nombre, data.marca, data.tipo, data.PVP)
+                handler.aniadir_producto_factura(params.id,product,data.cantidad)
+                reply.code(200).send({result: `Product with ID ${params.idp} in bill with ID ${params.id} updated successfully.`})
+            } catch {
+                reply.code(404).send({error: "Product and/or bill not found."})
+            }
+        }
     })
 
-    fastify.delete('/:id/product/:idp',async function (request, reply) {
-        
+    fastify.route({
+        method: 'GET',
+        url: '/:id/total',
+        schema: {
+            params: CV.BillID
+        },
+        handler: function (request, reply) {
+            let id = JSON.parse(JSON.stringify(request.params)).id
+            try {
+                let quantity = handler.calcular_total_factura(id)
+                reply.code(200).send({total: quantity})
+            } catch {
+                reply.code(404).send({error: `Bill with ID ${id} not found.`})
+            }
+        }
     })
 
-    fastify.put('/:id/product/:idp',async function (request, reply) {
-        
+    fastify.route({
+        method: 'PATCH',
+        url: '/:id/product/:idp',
+        schema: {
+            params: CV.ProductOfABillID,
+            body: CV.QuantityOfAProductData
+        }, 
+        handler: function (request, reply) {
+            let params = JSON.parse(JSON.stringify(request.params))
+            let data = JSON.parse(JSON.stringify(request.body))
+            try {
+                handler.actualizar_cantidad_producto_factura(params.id, params.idp, data.cantidad)
+                reply.code(200).send({result: `Quantity of product with ID ${params.idp} in bill with ID ${params.id} updated successfully.`})
+            } catch {
+                reply.code(404).send({error: "Product and/or bill not found"})
+            }
+        }
     })
 
-    fastify.get('/:id/total',async function (request, reply) {
-        
-    })
-
-    fastify.get('/:id/size',async function (request, reply) {
-        
-    })
 }
